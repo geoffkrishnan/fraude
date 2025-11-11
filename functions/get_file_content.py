861c52c11e61916fb37dc9139d69b96a2127b16e
Geoff Krishnan <geoffrykrishnan@gmail.com>
2025-11-11 16:08:59 -0600

    fix: Add exception handling to get_file_content func

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -13,16 +13,17 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(resolved_filepath):
         return f'Error: "{resolved_filepath}" is not found or is not a regular file: {resolved_filepath}'
 
-    with open(resolved_filepath, "r") as f:
-        char_count = sum(len(line) for line in f)
-        f.seek(0)
+    try:
+        with open(resolved_filepath, "r") as f:
+            char_count = sum(len(line) for line in f)
+            f.seek(0)
 
-        if char_count > MAX_CHARS:
-            file_content_string = f.read(MAX_CHARS)
-            file_content_string += (
-                f'[...File "{resolved_filepath}" truncated at {MAX_CHARS} characters]'
-            )
-            return file_content_string
-        else:
-            file_content_string = f.read()
-            return file_content_string
+            if char_count > MAX_CHARS:
+                file_content_string = f.read(MAX_CHARS)
+                file_content_string += f'[...File "{resolved_filepath}" truncated at {MAX_CHARS} characters]'
+                return file_content_string
+            else:
+                file_content_string = f.read()
+                return file_content_string
+    except Exception as e:
+        return f'Error reading file "{resolved_filepath}": as {e}'
